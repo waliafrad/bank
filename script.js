4b9ac0aca328efaed6c1954b3393327ad0e01211
waliafrad7@gmail.com <waliafrad7@gmail.com>
2022-09-05 16:24:26 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener("click", function(){
+    const loginArea = document.getElementById("login-area");
+    loginArea.style.display = "none";
+    
+});
